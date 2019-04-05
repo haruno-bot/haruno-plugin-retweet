96cf15974eb766a1f75dc407345c97d67b764b77
mzi <macchenjl@foxmail.com>
2019-04-05 22:11:06 +0800

    fix: logger

--- retweet.go
+++ retweet.go
@@ -100,15 +100,15 @@ func handleAvatar(id, name, url string, groupNums []int64) {
 	cqMsg := coolq.NewMessage()
 	section := coolq.NewTextSection(fmt.Sprintf("%s\n更新了头像\n", name))
 	cqMsg = coolq.AddSection(cqMsg, section)
-	logger.Service.Field("Plugin retweet").Infof("头像地址 = %s", url)
+	logger.Field("Plugin retweet").Infof("头像地址 = %s", url)
 	section = coolq.NewImageSection(url)
 	cqMsg = coolq.AddSection(cqMsg, section)
 	cqMsgTxt := string(coolq.Marshal(cqMsg))
-	logger.Service.Field("Plugin retweet").Infof("向酷Q发送 -> %s", cqMsgTxt)
+	logger.Field("Plugin retweet").Infof("向酷Q发送 -> %s", cqMsgTxt)
 	for _, groupID := range groupNums {
 		coolq.Client.SendGroupMsg(groupID, cqMsgTxt)
 	}
-	logger.Service.Field("Plugin retweet").Successf("成功转发了一条来自%s(%s)的头像更新信息", name, id)
+	logger.Field("Plugin retweet").Successf("成功转发了一条来自%s(%s)的头像更新信息", name, id)
 }
 
 // Load 插件加载
@@ -121,13 +121,13 @@ func (_plugin Retweet) Load() error {
 	_plugin.conn = &clients.WSClient{
 		Name: "Plugin retweet",
 		OnConnect: func(conn *clients.WSClient) {
-			logger.Service.Infof("%s 已经连接到转推api服务器", conn.Name)
+			logger.Infof("%s 已经连接到转推api服务器", conn.Name)
 		},
 		OnMessage: func(raw []byte) {
 			wsWrapper := new(kcwiki_msgtransfer_protobuf.Websocket)
 			err := proto.Unmarshal(raw, wsWrapper)
 			if err != nil {
-				logger.Service.Field("Plugin retweet").Errorf("%s", err.Error())
+				logger.Field("Plugin retweet").Errorf("%s", err.Error())
 				return
 			}
 			switch wsWrapper.GetProtoType() {
@@ -135,30 +135,30 @@ func (_plugin Retweet) Load() error {
 				wsSystem := new(kcwiki_msgtransfer_protobuf.WebsocketNonSystem)
 				err := proto.Unmarshal(wsWrapper.GetProtoPayload(), wsSystem)
 				if err != nil {
-					logger.Service.Field("Plugin retweet").Errorf("%s", err.Error())
+					logger.Field("Plugin retweet").Errorf("%s", err.Error())
 					return
 				}
-				logger.Service.Field("Plugin retweet").Successf("%s", wsSystem.GetData())
+				logger.Field("Plugin retweet").Successf("%s", wsSystem.GetData())
 			case kcwiki_msgtransfer_protobuf.Websocket_NON_SYSTEM:
 				wsNonSystem := new(kcwiki_msgtransfer_protobuf.WebsocketNonSystem)
 				err := proto.Unmarshal(wsWrapper.GetProtoPayload(), wsNonSystem)
 				if err != nil {
-					logger.Service.Field("Plugin retweet").Errorf("%s", err.Error())
+					logger.Field("Plugin retweet").Errorf("%s", err.Error())
 					return
 				}
 				if wsNonSystem.GetModule() != _plugin.module {
 					return
 				}
-				logger.Service.Field("Plugin retweet").Successf("%s", wsNonSystem.GetData())
+				logger.Field("Plugin retweet").Successf("%s", wsNonSystem.GetData())
 				msg := new(TweetMsg)
 				err = json.Unmarshal([]byte(wsNonSystem.GetData()), msg)
 				if err != nil {
-					logger.Service.Field("Plugin retweet").Errorf("%s", err.Error())
+					logger.Field("Plugin retweet").Errorf("%s", err.Error())
 					return
 				}
 				if !coolq.Client.IsAPIOk() {
 					if msg.Cmd == "1" || msg.Cmd == "2" {
-						logger.Service.Field("Plugin retweet").Errorf("一条来自%s的消息被弄丢了(因为api连接没有准备好)", msg.FromName)
+						logger.Field("Plugin retweet").Errorf("一条来自%s的消息被弄丢了(因为api连接没有准备好)", msg.FromName)
 					}
 					return
 				}
@@ -182,18 +182,18 @@ func (_plugin Retweet) Load() error {
 						cqMsg = coolq.AddSection(cqMsg, section)
 					}
 					cqMsgTxt := string(coolq.Marshal(cqMsg))
-					logger.Service.Field("Plugin retweet").Infof("向酷Q发送 -> %s", cqMsgTxt)
+					logger.Field("Plugin retweet").Infof("向酷Q发送 -> %s", cqMsgTxt)
 					for _, groupID := range groupNums {
 						coolq.Client.SendGroupMsg(groupID, cqMsgTxt)
 					}
-					logger.Service.Field("Plugin retweet").Successf("成功转发了一条来自%s(%s)的推文", msg.FromName, msg.FromID)
+					logger.Field("Plugin retweet").Successf("成功转发了一条来自%s(%s)的推文", msg.FromName, msg.FromID)
 				case "2": // 头像
 					handleAvatar(msg.FromID, msg.FromName, fmt.Sprintf("%s%s", _plugin.imageRoot, msg.Avatar), groupNums)
 				}
 			}
 		},
 		OnError: func(err error) {
-			logger.Service.Field("Plugin retweet").Errorf("%s", err.Error())
+			logger.Field("Plugin retweet").Errorf("%s", err.Error())
 		},
 	}
 	err = _plugin.conn.Dial(_plugin.url,
